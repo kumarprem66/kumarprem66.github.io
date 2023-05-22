c50f29b2588fc21466c1223aa72b17881a056823
Prem Kumar <finpre59@gmail.com>
2023-05-22 20:38:44 +0530

    com

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ GitHubCalendar(".calendar", "kumarprem66", { responsive: true });
 function resume(){
   window.open("https://drive.google.com/file/d/1JBlDoXxAZuX2nRA-VwA2UzlJNyCsH-Na/view?usp=sharing", "_blank");
 };
+
+
+// let header = document.querySelector("#nav-menu");
+// header.classList.toggle('sticky',window.scrollY>100)
+
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+// remove toggle icon and navbar when click navbar link
+
+window.onscroll = ()=>{
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
+
